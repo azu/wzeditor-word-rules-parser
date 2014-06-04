30c1bc33ac2b06840bef6833f3c729a073add4b3
azu <azuciao@gmail.com>
2014-06-04 21:24:19 +0900

    feat(cli): add cli as stream

---
+++ cli/cli.js
@@ -0,0 +1,16 @@
+"use strict";
+var parserStream = require("../lib/wzeditor-rules-stream");
+var es = require("event-stream");
+var typeName = require("type-name");
+process.stdin                        //connect streams together with `pipe`
+    .pipe(es.split())                  //split stream to break on newlines
+    .pipe(parserStream())
+    .pipe(es.map(function (data, cb) {
+        cb(null, JSON.stringify(data, function censor(key, value) {
+            if (typeName(value) === "RegExp") {
+                return String(value);
+            }
+            return value;
+        }, 4) + "\n");
+    }))
+    .pipe(process.stdout);
