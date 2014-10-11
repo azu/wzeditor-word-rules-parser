4a48a10a44bdcd5a63052884e5631fe8a206603f
azu <azuciao@gmail.com>
2014-10-12 01:45:35 +0900

    fix(example): fix path

--- readme-example.js
+++ readme-example.js
@@ -4,7 +4,7 @@
  */
 "use strict";
 var fs = require("fs");
-var parser = require("lib/wzeditor-rules-parser");
-var content = fs.readFileSync(__dirname + "./dictionary/WEB+DB PRESS用語統一ルール", "utf-8");
+var parser = require("./lib/wzeditor-rules-parser");
+var content = fs.readFileSync(__dirname + "/dictionary/WEB+DB PRESS用語統一ルール", "utf-8");
 var result = parser.parse(content);
 console.log(result);
