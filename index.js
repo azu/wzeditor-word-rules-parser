1f64f509bf0bb7c6f77057a3f401a4dae728197a
azu <azuciao@gmail.com>
2015-09-19 21:58:56 +0900

    feat(parser): add rawLineParse

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 module.exports = {
     parse: require("./lib/wzeditor-rules-parser").parse,
+    rawLineParse: require("./lib/wzeditor-rules-parser").rawLineParse,
     lineParse: require("./lib/wzeditor-rules-parser").lineParse,
     lineParseStream: require("./lib/wzeditor-rules-stream")
 };
